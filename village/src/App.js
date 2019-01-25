3409c37b9adbf6b13fc88a0de2f9def341ba1f62
ajfrierson <alvinfrierson@fuller.edu>
2019-01-25 09:47:49 -0800

    added a post request to app.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Nav from './components/Nav';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +17,19 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  addSmurf = (newSmurf) => {
+    axios
+        .post('http://localhost:3333/smurfs', newSmurf)
+        .then(response => {
+          this.setState({ smurfs : response.data })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+
+    this.props.history.push('/')
+  }
+
   render() {
     return (
       <div className="App">
